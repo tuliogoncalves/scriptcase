72101116681257ebfec565867ffcc17e25783fa6
Túlio Gonçalves <tulio.goncalves@infortechms.com.br>
2025-04-23 16:31:26 -0400

    setting nginx

---
+++ sail_builders/scripts/docker-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# We'll say that we are by default in dev
+if [ -d /etc/php/current ]; then
+    sed -i 's/^display_errors\s*=.*/display_errors = On/g' /etc/php/current/fpm/conf.d/30-custom-php.ini
+    sed -i 's/^max_execution_time\s*=.*/max_execution_time = -1/g' /etc/php/current/fpm/conf.d/30-custom-php.ini
+fi
+
+# When is laravel installation
+if [ -f artisan ]; then
+    cd storage
+    mkdir -p logs
+    mkdir -p framework
+    mkdir -p framework/cache
+    mkdir -p framework/cache/data
+    mkdir -p framework/sessions
+    mkdir -p framework/testing
+    mkdir -p framework/views
+    cd ..
+
+    chmod -R 777 storage bootstrap
+fi
+
+if [ -f /start.sh ]; then
+    echo
+    echo "--- Start custom script ---"
+    echo
+
+    exec /start.sh
+fi
+
+echo
+echo "----------------------------"
+echo "--- Entreypoint Finished ---"
+echo "----------------------------"
+echo
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
